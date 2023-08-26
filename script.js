3771528db3c9169b5ddc120959e0063aa60326c0
keshavjha1310 <keshavjhainft-@atharvacoe.ac.in>
2023-08-26 12:40:24 +0530

    Udated script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('myForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent default form submission
+
+        // Call your validation functions
+        var isValid = validateForm();
+        if (isValid) {
+            authorization();
+        }
+    });
+
+    document.getElementById('login').addEventListener('submit',function(event){
+        
+    })
+
 function validateForm() {
     // Get all input fields within the form
     var inputFields = document.querySelectorAll('input');
-console.log("clicked")
+
     // Iterate through the input fields and check if they are filled
     for (var i = 0; i < inputFields.length; i++) {
         if (inputFields[i].value.trim() === '') {
@@ -9,19 +24,44 @@ console.log("clicked")
             return false;
         }
     }
- // Check for numeric characters in username and names
- var firstName = document.querySelector('input[name="firstName"]').value;
- var middleName = document.querySelector('input[name="middleName"]').value;
- var lastName = document.querySelector('input[name="lastName"]').value;
 
- if (containsNumeric(firstName) && containsNumeric(middleName) && containsNumeric(lastName)) {
-     alert('Username and names should not contain numeric characters.');
-     return false;
- }
-    // If all fields are filled, the form is valid
-    return true;
+    // Check for numeric characters in username and names
+    var firstName = document.querySelector('input[name="firstName"]').value;
+    var middleName = document.querySelector('input[name="middleName"]').value;
+    var lastName = document.querySelector('input[name="lastName"]').value;
+
+    if (containsNumeric(firstName) || containsNumeric(middleName) || containsNumeric(lastName)) {
+        alert('Username and names should not contain numeric characters.');
+        return false;
+    }
+
+    // If all fields are filled and valid, the form is valid
+    return authorization();
 }
-function containsNumeric(value){
-      // Use a regular expression to check for numeric characters
-      return /\d/.test(value);
+
+function containsNumeric(value) {
+    // Use a regular expression to check for numeric characters
+    return /\d/.test(value);
 }
+
+function authorization() {
+    const usernameDB = [];
+    const passwordDB = [];
+
+    var username = document.getElementById('Username').value;
+    var password = document.getElementById('Password').value;
+
+    for (var i = 0; i < usernameDB.length; i++) {
+        if (usernameDB[i] === username) {
+            alert("Username Taken!");
+            return false;
+        }
+    }
+
+    usernameDB.push(username);
+    passwordDB.push(password);
+    
+    alert('Account created!');
+
+}
+});
